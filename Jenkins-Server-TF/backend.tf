14ad6ccd4aadefe6111dd0cebd973e63d8e32602
Vishwajeet Kore <vishwkore@gmail.com>
2026-02-02 00:35:24 +0530

    Sync local Terraform and Jenkins code

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "jenkins-tf-backend-vish"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
